0084e6e6c8814fe05c1ba664edf08b3fefa6dc5a
Mike Oxhuge <mikelitoris34@icloud.com>
2021-06-01 16:09:40 +0200

    commit on day2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
-import { Card } from 'react-bootstrap'
+import { Card } from "react-bootstrap";
 const Footer = () => {
-    return ( 
-        <div className="footer">
-           <Card.Footer className="text-muted">Can't have a nice footer out of the box with this stinky bootstrap</Card.Footer>
-        </div>
-     );
-}
- 
+  return (
+    <div className="footer">
+      <Card.Footer className="text-muted">
+        Can't have a nice footer out of the box with this stinky bootstrap
+      </Card.Footer>
+    </div>
+  );
+};
+
 export default Footer;
