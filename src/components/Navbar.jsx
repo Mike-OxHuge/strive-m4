eac3b7751377f55de7cba7b194ecf5f514cf7fed
hasham zafar <hasham307@gmail.com>
2021-06-04 14:12:36 +0200

    image issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { IoSearchSharp } from "react-icons/io5";
+import { FaSearch } from "react-icons/fa";
+import {BsBellFill} from "react-icons/bs";
 import { Nav, Navbar, Container, Dropdown } from "react-bootstrap";
 import "../App.css";
 const NavBar = () => {
@@ -7,19 +8,20 @@ const NavBar = () => {
       <Navbar expand="lg" id="navbar">
         <Container fluid>
           <Navbar.Brand href="#home">
-            <img src="../assets/logo.png" alt="logo" />
+            <img src="./logo.png" alt="logo" id="navbar1" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto" id="navbar">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">TV Shows</Nav.Link>
-              <Nav.Link href="#link">Movies</Nav.Link>
-              <Nav.Link href="#link">Recently Added</Nav.Link>
+            <Nav className="me-auto" >
+              <Nav.Link id="navbar" href="#home">Home</Nav.Link>
+              <Nav.Link id="navbar" href="#link">TV Shows</Nav.Link>
+              <Nav.Link id="navbar" href="#link">Movies</Nav.Link>
+              <Nav.Link id="navbar" href="#link">Recently Added</Nav.Link>
             </Nav>
-            <div className="ml-auto d-flex align-items-center ">
-              <IoSearchSharp />
-              <a href="#kids">Kids</a>
+            <div className="ml-auto d-flex align-items-center">
+              <FaSearch />
+              <a id="navbar1" href="#kids" className='mx-2'>Kids </a>
+              <BsBellFill/>
               <Dropdown>
                 <Dropdown.Toggle
                   id="dropdown-button-dark-example1"
