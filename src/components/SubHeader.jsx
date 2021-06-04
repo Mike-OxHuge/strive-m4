3a3977960765e00394f4eb0af24c6b3872278cde
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2021-06-04 12:03:36 +0200

    genres maped

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -3,7 +3,18 @@ import {Container, Row, Col, Dropdown, DropdownButton} from 'react-bootstrap'
 import { BsGrid, BsTextLeft } from "react-icons/bs"
 
 class SubHeader extends Component {
-  state = {};
+  state = { 
+    genres: ['Action', 'Drama', 'Comedy', 'Romance', 'Scifi', 'Fantasy', 'Documentary', 'Family'],
+    CurrentGenre: ''
+  };
+
+  // componentDidMount = async () => {
+  //   let response = await fetch(
+  //     `https://www.omdbapi.com/?apikey=2bc5c486&s=${this.state.CurrentGenre}`
+  //   );
+  //   let genres = await response.json();
+  // };
+
   render() {
     return (
       <> 
@@ -11,10 +22,13 @@ class SubHeader extends Component {
           <Row>
             <Col className = 'col-8 d-flex justify-content-center'>
               <h1 className = 'mr-5'>TV SHOWS</h1>
-              <DropdownButton id="dropdown-basic-button" title="Genres">
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              <DropdownButton id="dropdown-basic-button" title="Genres" variant = 'dark'>
+                {this.state.genres.map(genre => 
+                    <Dropdown.Item href="#/action-1">{genre}</Dropdown.Item>
+                    // this..setState({
+                    //   CurrentGenre: `${genre}`
+                    // })
+                    )}
               </DropdownButton>
             </Col>
             <Col className='col-4'>
