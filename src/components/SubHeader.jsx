a3880fcf2b7b614465157a2d496d9402e72542bd
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2021-06-04 10:58:23 +0200

    react-icon update

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -1,10 +1,28 @@
 import { Component } from "react";
+import {Container, Row, Col, Dropdown, DropdownButton} from 'react-bootstrap'
+import { BsGrid, BsTextLeft } from "react-icons/bs"
+
 class SubHeader extends Component {
   state = {};
   render() {
     return (
-      <>
-        <h1>Subheader</h1>
+      <> 
+        <Container>
+          <Row>
+            <Col className = 'col-8 d-flex justify-content-center'>
+              <h1 className = 'mr-5'>TV SHOWS</h1>
+              <DropdownButton id="dropdown-basic-button" title="Genres">
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
+            </Col>
+            <Col className='col-4'>
+              <BsGrid/>
+              <BsTextLeft/>
+            </Col>
+          </Row>
+        </Container>
       </>
     );
   }
