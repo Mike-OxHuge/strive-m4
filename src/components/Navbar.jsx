e33d3b06565500bf8cb1ae6d343bc43adb20c5a7
hasham zafar <hasham307@gmail.com>
2021-06-04 12:35:04 +0200

    icon issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,61 @@
-import { Nav, Navbar, Container } from "react-bootstrap";
-// removed NavDropDown, since it was never used
-const NavBar = () => {
-  return (
-    <>
-      <Navbar bg="dark" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#home">
-            <img src="../assets/logo.png" alt="logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">TV Shows</Nav.Link>
-              <Nav.Link href="#link">Movies</Nav.Link>
-              <Nav.Link href="#link">Recently Added</Nav.Link>
-            </Nav>
-            <div
-              className="ml-auto d-flex align-items-center dropdown-menu userDropdown"
-              aria-labelledby="navbarDropdownMenuLink"
-            >
-              <img src="./assets/kids_icon.png" alt="kidslogo" />
+import { IoSearchSharp } from 'react-icons/AiOutlineFileSearch '
+import {Nav,Navbar,Container,Dropdown} from 'react-bootstrap'
+import "../App.css"
+const NavBar =()=>  {
+  
 
-              <link className="dropdown-item" href="#">
-                {/* Kids <img class="kidsIcon" src="kids_icon.png" alt="kidsIcon" /> */}
-              </link>
-              <link className="addUser " href="profile.html">
-                {/* {" "}
-                Add User */}
-              </link>
-            </div>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    return (
+      
+      <>
+     <Navbar  expand="lg" id='navbar'>
+  <Container fluid>
+    <Navbar.Brand href="#home"><img src="../assets/logo.png" alt="logo" /></Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto" id='navbar'>
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">TV Shows</Nav.Link>
+        <Nav.Link href="#link">Movies</Nav.Link>
+        <Nav.Link href="#link">Recently Added</Nav.Link>
+       
+      </Nav> 
+      <div className='ml-auto d-flex align-items-center ' >
+      
+       <IoSearchSharp/>?
+          <a  href="#">
+                  Kids   
+                </a>
+               
+           
+                <Dropdown>
+    <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+      <img src="../assets/kids_icon.png" alt="img" />
+    </Dropdown.Toggle>
+
+    <Dropdown.Menu variant="dark">
+      <Dropdown.Item href="#" active>
+        Add User
+      </Dropdown.Item>
+      <Dropdown.Item href="#">Kids</Dropdown.Item>
+      
+      <Dropdown.Divider />
+      <Dropdown.Item href="#">Log out</Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
+                 
+              
+                
+               
+              
+         </div>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     </>
-  );
-};
+      
+      
+    )
+  }
+
 
 export default NavBar;
