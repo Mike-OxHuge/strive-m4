2c57574056ef59c2908b0be83634edc61ca1b5cb
Mike Oxhuge <mikelitoris34@icloud.com>
2021-06-04 15:49:58 +0200

    commit on day5tp -m infinite loop in searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Card } from "react-bootstrap";
 class SearchBar extends Component {
   state = {
     searchQuery: "",
@@ -15,21 +15,39 @@ class SearchBar extends Component {
     console.log(
       `The search result for a query of ${this.state.searchQuery} are:`
     );
+    this.setState({
+      movies: movies.Search,
+    });
     console.log(movies.Search);
   };
 
   render() {
     return (
-      <div>
+      <div className="mt-5">
         <Form.Group controlId="formGroupEmail">
-          {/* <Form.Label>Search</Form.Label> */}
+          <Form.Label>Search</Form.Label>
           <Form.Control
             type="text"
             placeholder="Search movie"
             value={this.state.searchQuery}
-            onChange={(e) => this.setState({ searchQuery: e.target.value })}
+            onChange={(e) =>
+              this.setState({
+                searchQuery: e.target.value,
+              })
+            }
           />
         </Form.Group>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
