28f648a783be711ab17901ffaa243422152dbc40
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2021-06-04 14:11:21 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,63 @@
 import { Component } from "react";
+import {Container, Row, Col, ListGroup } from 'react-bootstrap'
+import { IoLogoInstagram, IoLogoFacebook,IoLogoTwitter,IoLogoYoutube } from "react-icons/io5"
+import "../App.css";
+
 class Footer extends Component {
   state = {};
   render() {
     return (
       <>
-        <h1>Footer</h1>
+        <Container fluid className='footer'>
+          <div className="wrapper">
+            <Row>
+              <IoLogoFacebook/>
+              <IoLogoInstagram/>
+              <IoLogoTwitter/>
+              <IoLogoYoutube/>
+            </Row>
+            <Row className='listitems'>
+              <Col className ='col-3'>
+                <ListGroup style={{border: 'none', color: 'white', }}>
+                  <ListGroup.Item>Audio and Subtitles</ListGroup.Item>
+                  <ListGroup.Item>Media Center</ListGroup.Item>
+                  <ListGroup.Item>Privacy</ListGroup.Item>
+                  <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                  <ListGroup.Item>Contact us</ListGroup.Item>
+                </ListGroup>
+              </Col>
+              <Col className ='col-3'>
+                <ListGroup>
+                  <ListGroup.Item>Audio Description</ListGroup.Item>
+                  <ListGroup.Item>Investor Relastions</ListGroup.Item>
+                  <ListGroup.Item>Legal Notices</ListGroup.Item>
+                </ListGroup>
+              </Col>
+              <Col className ='col-3'>
+                <ListGroup>
+                  <ListGroup.Item>Help Center</ListGroup.Item>
+                  <ListGroup.Item>Jobs</ListGroup.Item>
+                  <ListGroup.Item>Cookie Preferences</ListGroup.Item>
+                </ListGroup>
+              </Col>
+              <Col className ='col-3'>
+                <ListGroup>
+                  <ListGroup.Item>Gift Cards</ListGroup.Item>
+                  <ListGroup.Item>Term of Use</ListGroup.Item>
+                  <ListGroup.Item>Corporate Information</ListGroup.Item>
+                </ListGroup>
+              </Col>
+            </Row>
+            <Row>
+              <h3>Service Code</h3>
+            </Row>
+            <Row>
+              <p>© 1997-2021 Netflix inc. </p>
+            </Row>
+          </div>
+
+          
+        </Container>
       </>
     );
   }
