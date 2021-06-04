ba3d6ce09219c54ab162251f6d8d8d359198545f
Mike Oxhuge <mikelitoris34@icloud.com>
2021-06-04 12:38:41 +0200

    commit on day5tp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,61 +1,49 @@
-import { IoSearchSharp } from 'react-icons/AiOutlineFileSearch '
-import {Nav,Navbar,Container,Dropdown} from 'react-bootstrap'
-import "../App.css"
-const NavBar =()=>  {
-  
+import { IoSearchSharp } from "react-icons/io5";
+import { Nav, Navbar, Container, Dropdown } from "react-bootstrap";
+import "../App.css";
+const NavBar = () => {
+  return (
+    <>
+      <Navbar expand="lg" id="navbar">
+        <Container fluid>
+          <Navbar.Brand href="#home">
+            <img src="../assets/logo.png" alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" id="navbar">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">TV Shows</Nav.Link>
+              <Nav.Link href="#link">Movies</Nav.Link>
+              <Nav.Link href="#link">Recently Added</Nav.Link>
+            </Nav>
+            <div className="ml-auto d-flex align-items-center ">
+              <IoSearchSharp />
+              <a href="#kids">Kids</a>
+              <Dropdown>
+                <Dropdown.Toggle
+                  id="dropdown-button-dark-example1"
+                  variant="secondary"
+                >
+                  <img src="../assets/kids_icon.png" alt="img" />
+                </Dropdown.Toggle>
 
-    return (
-      
-      <>
-     <Navbar  expand="lg" id='navbar'>
-  <Container fluid>
-    <Navbar.Brand href="#home"><img src="../assets/logo.png" alt="logo" /></Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto" id='navbar'>
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">TV Shows</Nav.Link>
-        <Nav.Link href="#link">Movies</Nav.Link>
-        <Nav.Link href="#link">Recently Added</Nav.Link>
-       
-      </Nav> 
-      <div className='ml-auto d-flex align-items-center ' >
-      
-       <IoSearchSharp/>?
-          <a  href="#">
-                  Kids   
-                </a>
-               
-           
-                <Dropdown>
-    <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-      <img src="../assets/kids_icon.png" alt="img" />
-    </Dropdown.Toggle>
+                <Dropdown.Menu variant="dark">
+                  <Dropdown.Item href="#" active>
+                    Add User
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#">Kids</Dropdown.Item>
 
-    <Dropdown.Menu variant="dark">
-      <Dropdown.Item href="#" active>
-        Add User
-      </Dropdown.Item>
-      <Dropdown.Item href="#">Kids</Dropdown.Item>
-      
-      <Dropdown.Divider />
-      <Dropdown.Item href="#">Log out</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
-                 
-              
-                
-               
-              
-         </div>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+                  <Dropdown.Divider />
+                  <Dropdown.Item href="#">Log out</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
-      
-      
-    )
-  }
-
+  );
+};
 
 export default NavBar;
--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -1,11 +1,20 @@
 import { Component } from "react";
-import {Container, Row, Col, Dropdown, DropdownButton} from 'react-bootstrap'
-import { BsGrid, BsTextLeft } from "react-icons/bs"
+import { Container, Row, Col, Dropdown, DropdownButton } from "react-bootstrap";
+import { BsGrid, BsTextLeft } from "react-icons/bs";
 
 class SubHeader extends Component {
-  state = { 
-    genres: ['Action', 'Drama', 'Comedy', 'Romance', 'Scifi', 'Fantasy', 'Documentary', 'Family'],
-    CurrentGenre: ''
+  state = {
+    genres: [
+      "Action",
+      "Drama",
+      "Comedy",
+      "Romance",
+      "Scifi",
+      "Fantasy",
+      "Documentary",
+      "Family",
+    ],
+    CurrentGenre: "",
   };
 
   // componentDidMount = async () => {
@@ -17,23 +26,29 @@ class SubHeader extends Component {
 
   render() {
     return (
-      <> 
+      <>
         <Container>
           <Row>
-            <Col className = 'col-8 d-flex justify-content-center'>
-              <h1 className = 'mr-5'>TV SHOWS</h1>
-              <DropdownButton id="dropdown-basic-button" title="Genres" variant = 'dark'>
-                {this.state.genres.map(genre => 
+            <Col className="col-8 d-flex justify-content-center">
+              <h1 className="mr-5">TV SHOWS</h1>
+              <DropdownButton
+                id="dropdown-basic-button"
+                title="Genres"
+                variant="dark"
+              >
+                {this.state.genres.map(
+                  (genre) => (
                     <Dropdown.Item href="#/action-1">{genre}</Dropdown.Item>
-                    // this..setState({
-                    //   CurrentGenre: `${genre}`
-                    // })
-                    )}
+                  )
+                  // this..setState({
+                  //   CurrentGenre: `${genre}`
+                  // })
+                )}
               </DropdownButton>
             </Col>
-            <Col className='col-4'>
-              <BsGrid/>
-              <BsTextLeft/>
+            <Col className="col-4">
+              <BsGrid />
+              <BsTextLeft />
             </Col>
           </Row>
         </Container>
